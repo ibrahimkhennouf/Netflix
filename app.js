f726b8e8645e528485e43a9bc5b3dd188f9b5569
ibrahimkhennouf <ibahimkhennouf10@gmail.com>
2022-01-17 11:40:13 +0100

    initial

---
+++ app.js
@@ -0,0 +1,8 @@
+let tglshow = () => {
+  let elm = document.getElementById("cont");
+  if (elm.style.display == "none") {
+    elm.style.display = "block";
+  } else {
+    elm.style.display = "none";
+  }
+};
